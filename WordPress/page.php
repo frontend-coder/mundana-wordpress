939f1864dab40be1e3d36b7b2e97ffafe4232382
frontendercode <frontendercode@gmail.com>
2022-07-07 20:49:00 +0300

    v.0.24
    Зробив шаблон сторінки теми

--- WordPress/page.php
+++ WordPress/page.php
@@ -15,24 +15,23 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+  <?php if ( have_posts() ) : ?>
+  <?php while ( have_posts() ) : 	the_post(); ?>
+  <!--------------------------------------
+HEADER
+--------------------------------------->
 
-			get_template_part( 'template-parts/content', 'page' );
+  <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+  <?php endwhile;
+	      endif; ?>
 
-		endwhile; // End of the loop.
-		?>
+  <!-- End Main -->
 
-	</main><!-- #main -->
+</main>
+
 
 <?php
-get_sidebar();
 get_footer();
